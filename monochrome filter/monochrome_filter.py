261314c7cc5525e16197c1adb0758dce969f12ba
Marshanah T. ♡ <marshanah.taylor@utexas.edu>
2023-03-10 23:43:28 -0600

    Add files via upload

---
+++ monochrome filter/monochrome_filter.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Exercise A: Old-School Filter (Monochrome Beastars 🐺 Manga)
+
+# My monochrome_filter() function transforms this image of the wolf character Legoshi from the manga "Beastars" into a formidable beast by adding a monochrome effect to his appearance.
+
+# In[189]:
+
+
+from PIL import Image
+
+
+# In[190]:
+
+
+import PIL.ImageOps
+
+def monochrome_filter(monochrome):
+    img = Image.open('legoshi.png')
+    rgbimage = img.convert('RGB')
+    monochrome = PIL.ImageOps.invert(rgbimage)
+    monochrome.save('monochrome.png')
+    return monochrome
+
+monochrome_filter(monochrome_filter)
+
+
+# In[ ]:
+
+
+
+
