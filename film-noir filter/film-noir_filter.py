695bfd4d7c15ed3139031d8dbfbbd0af017dbbdd
Marshanah T. ♡ <marshanah.taylor@utexas.edu>
2023-03-12 10:16:40 -0500

    Update film-noir_filter.py

--- film-noir filter/film-noir_filter.py
+++ film-noir filter/film-noir_filter.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 # # Filter #2: Film Noir 🕵🏾‍♀️
 
 # My film_noir() function transforms this image of the Strawhat Pirates from the manga "One Piece" by creating a black-and-white, vintage style effect similar to the Grayscale filter in Photoshop. 
 
-# In[54]:
-
-from PIL import Image
-
-# In[55]:
-
 import PIL.ImageOps
-
-# In[56]:
+from PIL import Image
 
 def film_noir(img):
     img = Image.open('strawhat pirates.png')
@@ -23,7 +14,5 @@ def film_noir(img):
     film_noir = PIL.ImageOps.grayscale(rgbimage)
     film_noir.save('film_noir.png')
     return film_noir
-
-# In[57]:
 
 film_noir(film_noir)
