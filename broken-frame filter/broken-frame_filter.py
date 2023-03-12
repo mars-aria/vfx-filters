2bc0380330cd60b34ccb9f3f99da497b12d6d1b2
Marshanah T. ♡ <marshanah.taylor@utexas.edu>
2023-03-12 10:19:29 -0500

    Update broken-frame_filter.py

--- broken-frame filter/broken-frame_filter.py
+++ broken-frame filter/broken-frame_filter.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-#!/usr/bin/env python
-# coding: utf-8
-
 # # Filter #1: Broken Frame 🔨
 
 # My broken_frame() function creates a fractured effect in the image below that I liken in appearance to the Distorted Glass filter in Photoshop. 
 
-# In[49]:
-
+import PIL.ImageOps
 from PIL import Image
-
-# In[50]:
 
 def broken_frame(image):
     length = image.size[1]
@@ -25,15 +17,9 @@ def broken_frame(image):
             image.putpixel((length - 1 - x, y), left)
             image.putpixel((x, y), right)
 
-# In[51]:
-
 testimage = Image.open('black girls in ux.png') 
 
-# In[52]:
-
 testimage
-
-# In[53]:
 
 broken_frame(testimage)
 testimage
